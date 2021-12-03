475f7781eef7ef8790608583dde55a2c55a19428
Araz Al Hamdani <aalhamdani.mmt-b2020@fh-salzburg.ac.at>
2021-12-03 01:10:10 +0100

    init empty template site

---
+++ src/pages/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("php/head.php"); ?>
+</head>
+<body>
+    <main>
+       
+    </main>
+    <footer>
+        <section id="footer-section">
+            <h2>Where to find me</h2>
+            <?php include("php/socials-nav.php"); ?>
+        </section>
+        <script src="js/script.js"></script>
+    </footer>
+</body>
+</html>
---
+++ src/pages/php/head.php
@@ -0,0 +1,45 @@
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>araz</title>
+
+<script type="module">
+    document.documentElement.classList.remove('no-js');
+    document.documentElement.classList.add('js');
+</script>
+
+<link rel="stylesheet" href="style.css" />
+
+
+<meta
+    name="description"
+    content="URL Shortner made for my self."
+/>
+<meta property="og:title" content="sh.araz.dev" />
+<meta
+    property="og:description"
+    content="URL Shortner made for my self."
+/>
+<meta
+    property="og:image"
+    content="https://www.araz.dev/media/logo.png"
+/>
+<meta property="og:image:alt" content="araz Logo" />
+<meta property="og:locale" content="de_AT" />
+<meta property="og:type" content="website" />
+<meta name="twitter:card" content="summary" />
+<meta property="og:url" content="https://www.sh.araz.dev" />
+<link rel="canonical" href="https://www.sh.araz.dev" />
+
+<link rel="icon" href="favicons/favicon.ico" />
+
+<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+<link rel="icon" type="image/svg+xml" href="favicons/favicon-w.svg">
+<link rel="manifest" href="favicons/site.webmanifest">
+<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
+
+<meta name="theme-color" content="#000000" />
+
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap" rel="stylesheet"> 
